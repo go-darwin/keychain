6202af255c6ef7aefa5f8a7716d6b0d01e3110ba
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-23 11:49:35 +0900

    keychain: add initial import keychain package
    Imported by rsc.io/rsc/keychain.
    
    rsc said on README:
    It is NOT expected that code here can be used directly. Please don't.
    
    Just copied and small fix the named return variables.
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2017 The go-darwin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package keychain implements access to the passwords and other keys
+// stored in the system-provided keychain.
+package keychain
+
+// BUG(zchee): Package keychain now only implemented on macOS.
---
+++ keychain.go
@@ -0,0 +1,22 @@
+// Copyright 2017 The go-darwin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package keychain
+
+import "fmt"
+
+// UserPasswd returns the user name and password for authenticating
+// to the named server. If the user argument is non-empty, UserPasswd
+// restricts its search to passwords for the named user.
+func UserPasswd(server, preferredUser string) (string, string, error) {
+	user, passwd, err := userPasswd(server, preferredUser)
+	if err != nil {
+		if preferredUser != "" {
+			return "", "", fmt.Errorf("error: loading password for %s@%s: %v", preferredUser, server, err)
+		} else {
+			return "", "", fmt.Errorf("error: loading password for %s: %v", server, err)
+		}
+	}
+	return user, passwd, nil
+}
