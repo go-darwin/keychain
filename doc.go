2f25b47752f7e3c08fb3869f78fdc6736faa3b19
Koichi Shiraishi <zchee.io@gmail.com>
2019-05-22 18:53:57 +0900

    all: add canonical import path

--- doc.go
+++ doc.go
@@ -4,6 +4,6 @@
 
 // Package keychain implements access to the passwords and other keys
 // stored in the system-provided keychain.
-package keychain
+package keychain // import "go-darwin.dev/keychain"
 
 // BUG(zchee): Package keychain now only implemented on macOS.
