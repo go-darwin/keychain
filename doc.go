545c7928a404ee2dfb5cc9ed07eabab6370264bd
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-11 22:20:21 +0900

    all: rollback import path to github.com/go-darwin
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- doc.go
+++ doc.go
@@ -4,6 +4,6 @@
 
 // Package keychain implements access to the passwords and other keys
 // stored in the system-provided keychain.
-package keychain // import "go-darwin.dev/keychain"
+package keychain // import "github.com/go-darwin/keychain"
 
 // BUG(zchee): Package keychain now only implemented on macOS.
